6756353cfa203e379fd864fb4df0f9e7d81bd2b5
Lucas Zipp <lucas.zipp@gmail.com>
2016-06-12 13:21:57 -0700

    changes some filenames and deleted some files

--- popup.js
+++ popup.js
@@ -1,8 +1,40 @@
-// var spoilers={"spo"}
 var spoiler_word_arr=[]
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function del_spoiled_word(num) {
+    spoiler_word_arr.splice(num,1);
+    console.log('spliced')
+    console.log(spoiler_word_arr)
+    console.log('num='+num)
+    renderStatus(spoiler_word_arr );
+    var popup_message={'tag': 'popup', 'spoiled_words':spoiler_word_arr}
+chrome.runtime.sendMessage({greeting: popup_message}, function(response) {
+  console.log('sent_popup_info');
+});
+}  
+
+
+function renderStatus(spoils) {
+  //document.getElementById('status').textContent = statusText;
+      var element=document.getElementById("status");
+element.innerHTML = '';
+  var i;
+  for (i=0; i<spoils.length; i +=1){
+    var div = document.createElement("div");
+    div.classList.add("spoiled_word_div")
+    div.innerHTML = spoils[i];
+    var btn_close=document.createElement("BUTTON")
+    function wrapper(num) {
+      return function() {del_spoiled_word(num)}
+    }
+    var del_spoiled_word_set= wrapper(i)
+    btn_close.onclick = del_spoiled_word_set;
+
+  var t = document.createTextNode("X");     
+    btn_close.classList.add("button_close")
+    btn_close.appendChild(t);
+    div.appendChild(btn_close) 
+     element.appendChild(div)
+  }
 }
 
 function contains(arr, word) {
@@ -21,23 +53,22 @@ var submitForm = function() {
   console.log("Word to block: " + spoiler_word)
   spoiler_word_arr.push(spoiler_word)
 }
- console.log(spoiler_word_arr)
-  renderStatus('Spoiler blocking for: '+ spoiler_word_arr );
+  renderStatus(spoiler_word_arr );
 
-
-    chrome.extension.sendMessage({greeting: "spoil_list"+spoiler_word_arr},
-        function (response) {
-         console.log(response.farewell);
-           
-        });
-
-
-}
-
+var popup_message={'tag': 'popup', 'spoiled_words':spoiler_word_arr}
+chrome.runtime.sendMessage({greeting: popup_message}, function(response) {
+  console.log('sent_popup_info');
+});
+};
 
 document.addEventListener('DOMContentLoaded', function() {
 console.log('loaded')
-  renderStatus('loaded');
+var contact_message={'tag': 'load_from_background'}
+    chrome.extension.sendMessage({greeting: contact_message},
+        function (response) {
+         spoiler_word_arr=response.farewell;
+         renderStatus(spoiler_word_arr );
+        });
 })
 document.getElementById("sub").addEventListener("click", submitForm);
 
